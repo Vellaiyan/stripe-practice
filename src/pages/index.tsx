20cece7a569a06485d0f3ed96e10ff0e838a6667
vellaiyan <vellaiyan211464@gmail.com>
2023-02-13 11:45:01 +0530

    resolve conflicts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
-import checkout from "./checkout";
+import checkout from "../components/checkout";
 
 const inter = Inter({ subsets: ["latin"] });
 
